b150791748e58d16e7eb661c8d9ca9da1db3496b
Jmcclain0129 <jmcclain0129@gmail.com>
2019-07-22 15:34:52 +0100

    Added the ability to write to and to read from a list and display the messages to the user

--- run.py
+++ run.py
@@ -1,30 +1,42 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
 
-@app.route("/")
+
+def add_messages(username, message):
+    """Add messages to the 'messages' list"""
+    messages.append("{}: {}".format(username, message))
+
+
+def get_all_messages():
+    """Get all messages and separate them with a 'br'"""
+    return "<br>".join(messages)
+
+
+@app.route('/')
 def index():
     """Main page with instructions"""
     return "To send a message use /USERNAME/MESSAGE"
 
-@app.route('/username')
+
+@app.route('/<username>')
 def user(username):
-    return "hi" + username
+    """Display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return"{0}: {1}".format(username, message)
-
-
-
-
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 # app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
 
 if __name__ == '__main__':
- app.run(host=os.environ.get('IP', '127.0.0.1'),
-         port=int(os.environ.get('PORT', '8080')),
-         debug=True)
-
+    app.run(host=os.environ.get('IP', '127.0.0.1'),
+            port=int(os.environ.get('PORT', '8080')),
+            debug=True)
